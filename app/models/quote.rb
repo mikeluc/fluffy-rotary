9b60ebeb7f736f5fa2d490e8c7c0f0ff50da3892
Miguel Lucas <miguelluc@ymail.com>
2017-08-03 06:16:26 +0000

    implemented validation for both the saying and the author also coded an error notification

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ApplicationRecord
+
+  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :author, presence: ture, length: { maximum: 50, minimum: 3 }
+
 end
