b48993cda47486f75cdc2e53c7c6aca8a47363ce
Miguel Lucas <miguelluc@ymail.com>
2017-08-03 02:29:15 +0000

    added the fonts

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,13 @@ Bundler.require(*Rails.groups)
 module Splurty
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
+    # Application configuration should go into files in config/initializers  
     # -- all .rb files in that directory are automatically loaded.
+    
+    # add fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
